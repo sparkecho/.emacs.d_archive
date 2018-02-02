cb11e7ca7ebc41197fa7872f4162c3265f24205d
sparkecho <echozhz@126.com>
2018-02-02 13:56:28 +0800

    Use use-package manage conf files.

--- conf/package-conf.el
+++ conf/package-conf.el
@@ -4,13 +4,31 @@
 
 ;;; Code:
 
-(require 'package)
+(eval-when-compile (require 'custom-conf))
+
+;; DO NOT copy package-selected-packages to init/custom file forcibly.
+;; https://github.com/seagle0128/.emacs.d/blob/master/lisp/init-package.el
+(with-eval-after-load 'package
+  (defun package--save-selected-packages (&optional value)
+    "Set and (don't!) save `package-selected-packages' to VALUE."
+    (when value
+      (setq package-selected-packages value))
+    (unless after-init-time
+      (add-hook 'after-init-hook #'package--save-selected-packages))))
 
 (setq package-archives '(("gnu"   . "https://elpa.emacs-china.org/gnu/")
                          ("melpa" . "https://elpa.emacs-china.org/melpa/")))
 
-;; (eval-after-load "package" '(package-initialize))
+(setq package-enable-at-startup nil)
 (package-initialize)
+
+;; Setup `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
 
 (provide 'package-conf)
 ;;; package-conf.el ends here
