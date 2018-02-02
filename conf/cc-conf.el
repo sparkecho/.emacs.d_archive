a6278a7dc43e90b7e8ff34afb313fc66ca8782e1
sparkecho <echozhz@126.com>
2018-02-02 13:58:47 +0800

    Use use-package manage irony.

--- conf/cc-conf.el
+++ conf/cc-conf.el
@@ -87,22 +87,55 @@
 ;; (semantic-add-system-include "/usr/include/GLFW" 'c++-mode)
 
 
-;; (global-set-key "\t" 'company-complete-common)
-;; (define-key company-mode-map "\t" nil)
-;; (define-key company-mode-map [(backtab)] 'company-complete-common)
-(add-hook 'c++-mode-hook 'company-mode)
-(eval-after-load 'company
-  '(progn
-     (setq company-idle-delay 0.2)
-     (add-to-list 'company-backends
-                  '(company-irony-c-headers company-irony))))
+;; ;; (global-set-key "\t" 'company-complete-common)
+;; ;; (define-key company-mode-map "\t" nil)
+;; ;; (define-key company-mode-map [(backtab)] 'company-complete-common)
+;; (add-hook 'c++-mode-hook 'company-mode)
+;; (eval-after-load 'company
+;;   '(progn
+;;      (setq company-idle-delay 0.2)
+;;      (add-to-list 'company-backends
+;;                   '(company-irony-c-headers company-irony))))
+;; (add-hook 'c++-mode-hook  'irony-mode)
+;; (add-hook 'c-mode-hook    'irony-mode)
+;; (add-hook 'objc-mode-hook 'irony-mode)
+;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
-(add-hook 'c++-mode-hook  'irony-mode)
-(add-hook 'c-mode-hook    'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
+;; irony-mode
+(use-package irony
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'c++-mode-hook  'irony-mode)
+  (add-hook 'c-mode-hook    'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+  :config
+  ;; replace the `completion-at-point' and `complete-symbol' bindings in
+  ;; irony-mode's buffers by irony-mode's function
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
 
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
+;; ;; company-mode
+;; (use-package company
+;;   :ensure t
+;;   :defer t
+;;   :init
+;;   (add-hook 'after-init-hook 'global-company-mode)
+;;   :config
+;;   (add-hook 'c++mode-hook
+;;             (lambda ()
+;;               (use-package company-irony :ensure t :defer t)))
+;;   (setq company-idle-delay             nil
+;;         company-minimum-prefix-length  2
+;;         company-show-numbers           t
+;;         company-tooltip-limit          20)
+;;   (add-to-list 'company-backends
+;;                '((company-irony company-irony-c-headers))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Code Refactor
