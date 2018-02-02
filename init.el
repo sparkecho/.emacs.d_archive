1190d908f45372d54ec60419d068d40ded472a26
sparkecho <echozhz@126.com>
2018-02-02 15:25:33 +0800

    Rearrange.

--- init.el
+++ init.el
@@ -2,100 +2,39 @@
 ;;; Commentary:
 ;;; Code:
 
-
-;; Configuration of Load Path
-;; Added at 2016/4/16 -- 23:30
+;; Load Path
 (add-to-list 'load-path (expand-file-name "conf" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "plugins" user-emacs-directory))
 
-
+;; Customization
 (require 'custom-conf)
 
+;; Packages
 (package-initialize)
-
-;; Configuration of Package (mainly Elpa/Melpa)
-;; Added at 2016/4/17 -- 12:53
 (require 'package-conf)
 
-
-;; Configuration of Emacs's appearence
-;; Added at 2016/4/17 -- 12:29
+;; Preferences
 (require 'appearence-conf)
-
-
-;; Configuration of Emacs's Short Cut keys
-;; Added at 2016/4/17 -- 12:35
 (require 'shortcut-keys-conf)
-
-
-;; Configuration of Programming General Setting
-;; Added at 2018/1/11 -- 11:48
 (require 'general-conf)
-
-
-;; Configuration of ecb 代码浏览设置
-;; Added at 2016/5/23 -- 19:22
 (require 'ecb-conf)
-
-
-;; Configuration of gdb Debug tool
-;; Added at 2018/1/27 -- 19:49
 (require 'gdb-conf)
-
-
 (require 'markdown-conf)
 (require 'org-conf)
 
-
-;; Configuration of the Development Enviornment of Common Lisp
-;; Added at 2016/4/17 -- 11:00
+;; Programming
 (require 'common-lisp-conf)
-
-
-;; Configuration of C/C++ language
-;; Added at 2016/4/17 -- 11:03
 (require 'cc-conf)
-
-
-;; Configuration of Asm
-;; Added at 2016/7/6 -- 9:13
 (require 'asm-conf)
-
-
-;; Configuration of the Development Enviornment of (GNU) Smalltalk
-;; Added at 2016/4/17 -- 12:20
 ;; (require 'smalltalk-conf)
-
-
-;; Configuration of the Development Environment of Scala
-;; Added at 2016/4/17 -- 20:02
 ;; (require 'scala-conf)
-
-
-;; Configuration of the Development Environment of JavaScript
-;; Added at 2016/4/17 -- 12:38
 ;; (require 'javascript-conf)
-
-
-;; Configuration of the Development Environment of Octave
-;; Added at 2016/4/17 -- 12:40
 ;; (require 'octave-conf)
-
-
-;; Configuration of the Environment of TeX/LaTeX
 (require 'auctex-conf)
 
-
-;; Configuration of auto-complete
-;; Added at 2016/4/16 -- 16:45
-;; Deleted at 2018/1/11 -- 1:55
+;; Others
 ;; (require 'auto-complete-conf)
-
-
-;; Configuration of esup
-;; Added at 2016/4/17 -- 13:08
 ;; (require 'esup-conf)
-
 
 ;; Configuration of helm
 ;; Added at 2016/4/17 -- 10:36
